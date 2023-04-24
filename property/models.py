7837bec3cfd6f2ff06f0c8c219d0b8581e3e2831
Константин Смуров <Phantom2525@gmail.com>
2023-04-24 13:29:15 +0300

    add field Flat.owner_pure_phone

--- property/models.py
+++ property/models.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
+    owner_pure_phone = PhoneNumberField(
+        'Нормализованный номер владельца',
+        blank=True,
+        null=True,
+        region='RU',
+        db_index=True
+    )
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     new_building = models.BooleanField(null=True, blank=True, default=None)
     created_at = models.DateTimeField(
